176532aa110a8d71c7b06e7d02f3038d7a2bb843
TTHookGen <urbanlegends765@gmail.com>
2026-07-30 21:53:49 -0400

    Fix: Correct EdgeToEdgePlugin package name in MainActivity.java to resolve compilation errors

--- android/app/src/main/java/fun/playnpayday/MainActivity.java
+++ android/app/src/main/java/fun/playnpayday/MainActivity.java
@@ -2,7 +2,7 @@ package fun.playnpayday;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import io.capawesome.capacitor.plugins.edgetoedge.EdgeToEdgePlugin;
+import io.capawesome.capacitorjs.plugins.androidedgetoedgesupport.EdgeToEdgePlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
